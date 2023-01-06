c4b371b4aa32d5c785ccc8bf8565f74375c62f5a
Nicolò Rosati <nico.rosati03@gmail.com>
2023-01-06 17:18:53 +0100

    Refactor

--- Process/ese125.py
+++ Process/ese125.py
@@ -1,9 +1,12 @@
 f1=open("t1","rb")
 f2=open("t2","wb")
-for i in range(200):
+for i in range(2500):
     s=f1.readline()
     print(s)
     index=s.find(b"for: ")
+    flag= s.find(b"pwn")
+    if flag != -1:
+        break
     if index != -1:
         t1=s[index+4:-1]
         print(t1)

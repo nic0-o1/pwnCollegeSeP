66e451281987234edc3bfe4059f5d4db8714d331
Nicolò Rosati <nico.rosati03@gmail.com>
2023-01-08 21:29:19 +0100

    133 solution

---
+++ Process/ese133.py
@@ -0,0 +1,6 @@
+# WELCOME! This challenge makes the following asks of you:
+# - the challenge checks for a specific parent process : python
+# - the challenge will require the parent to send number of signals : 500
+
+import subprocess
+subprocess.run('/challenge/embryoio_level133')

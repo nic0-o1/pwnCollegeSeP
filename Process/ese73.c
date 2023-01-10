6785b25960dea3f5e96e50b230fb1b3e1fedfd91
Nicolò Rosati <nico.rosati03@gmail.com>
2023-01-10 21:10:14 +0100

    73 solution

---
+++ Process/ese73.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+void pwncollege(char *args[], char *env[]){
+
+    return;
+}
+
+int main(int argc, char* argv[], char* env[]){
+   chdir("/tmp/yppaqz");
+   execve("/challenge/embryoio_level73", argv,env);
+}
---
+++ Process/ese73.sh
@@ -0,0 +1,8 @@
+# WELCOME! This challenge makes the following asks of you:
+# - the challenge checks for a specific parent process : shellscript
+# - the challenge will check that it is running in a specific current working directory : /tmp/yppaqz
+# - the challenge will check to make sure that the parent's parent CWD to be different than the challenge's CWD
+
+#! /usr/bin/sh
+
+./a.out
